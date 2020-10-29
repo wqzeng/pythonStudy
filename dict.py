675d343d8b3bb254e9392a78173cc99858fd8f7a
moke.tsang <moke.tsang@vipshop.com>
2020-10-29 15:31:52 +0800

    rename

--- dict.py
+++
@@ -1,8 +0,0 @@
-#dict是键值对存储，类似map
-d = {'Michael': 95, 'Bob': 75, 'Tracy': 85}
-print(d['Bob'])
-
-if 'Michael' in d :
-    d.pop('Michael')
-
-print(d)
---
+++ forTest.py
@@ -0,0 +1,15 @@
+names = ['Michael', 'Bob', 'Tracy']
+for name in names:
+    print(name)
+sum = 0
+#range()生成整数队列
+for x in range(101):
+    sum = sum + x
+print('sum=',sum)
+
+sum = 0
+n = 100
+while n > 0:
+    sum = sum + n
+    n = n - 2
+print(sum)
